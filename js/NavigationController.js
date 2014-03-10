18fedcfc58bab333807e4de599e0a50116dad059
bweldy1 <bweldy1@gmail.com>
2014-03-09 22:54:29 -0400

    more awesome

--- js/NavigationController.js
+++ js/NavigationController.js
@@ -1,8 +1,22 @@
 function NavigationController($scope){
 
-	$scope.visible = false;
-
+	$scope.navVisible = false;
+	$scope.navTarget = "home";
+	
 	$scope.toggleVisible = function () {
-	    $scope.visible = !$scope.visible;
+	
+	    $scope.navVisible = !$scope.navVisible;
 	};
+	
+	$scope.isTarget = function(t)
+	{
+		return $scope.navTarget === t;
+	};
+	
+	$scope.navClick = function(n)
+	{
+		$scope.navTarget = n;
+		$scope.navVisible = false;
+	};
+
 };
